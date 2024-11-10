280e9594745d33751f4316d0b152c4c99cf5e4d4
dinhthituyetchinh <fengdinh@gmail.com>
2024-11-10 12:27:57 +0700

    GUI Product - Blog

---
+++ HighLandCoffeeWebsite/Controllers/BlogController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HighLandCoffeeWebsite.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        public ActionResult ViewBlog()
+        {
+            return View();
+        }
+    }
+}
