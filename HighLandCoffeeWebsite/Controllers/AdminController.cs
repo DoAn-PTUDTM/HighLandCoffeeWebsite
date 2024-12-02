378c0ae45cb321d44d168ba329c639eca48caea0
dinhthituyetchinh <fengdinh@gmail.com>
2024-12-02 18:54:59 +0700

    Admin Update

--- HighLandCoffeeWebsite/Controllers/AdminController.cs
+++ HighLandCoffeeWebsite/Controllers/AdminController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HighLandCoffeeWebsite.Models;
+using HighLandCoffeeWebsite.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,75 @@ namespace HighLandCoffeeWebsite.Controllers
 {
     public class AdminController : Controller
     {
+        AdminService adminService = new AdminService();
         // GET: AdminHome
         public ActionResult Index()
         {
             return View();
         }
+
+        public ActionResult ViewProduct()
+        {
+            try
+            {
+
+                ViewData["Err"] = TempData["Err"];
+                List<Admin_Product> p = adminService.ExcuteSQL();
+                return View(p);
+            }
+            catch (Exception ex)
+            {
+                return Content(ex.Message);
+            }
+        }
+
+        public ActionResult AddProduct()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddProduct(Admin_Product product)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    adminService.AddSql(product.productName, product.productDescription, product.price, product.images, product.productTypeID);
+                    return RedirectToAction("ViewProduct");
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            catch (Exception ex)
+            {
+                return Content(ex.Message);
+            }
+
+        }
+
+        public ActionResult DeleteProduct(int id)
+        {
+            try
+            {
+                if (!adminService.checkProductInCart(id))
+                {
+                    adminService.DeleteSql(id);
+                    return RedirectToAction("ViewProduct");
+                }
+                else
+                {
+                    TempData["Err"] = "Hiện tại chưa xoá được sản phẩm";
+                    return RedirectToAction("ViewProduct");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return Content(ex.Message);
+            }
+        }
+
     }
 }
---
+++ HighLandCoffeeWebsite/Services/AdminService.cs
@@ -0,0 +1,119 @@
+﻿using HighLandCoffeeWebsite.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+
+namespace HighLandCoffeeWebsite.Services
+{
+    public class AdminService
+    {
+        public string conStr = "Data Source=TUYETCHINH;Initial Catalog=HIGHLANDCOFFEE;PersistSecurityInfo=True;User ID=sa;Password=123456;TrustServerCertificate=True";
+        public List<Admin_Product> ExcuteSQL()
+        {
+            List<Admin_Product> productList = new List<Admin_Product>();
+
+            using (SqlConnection connection = new SqlConnection())
+            {
+                connection.ConnectionString = conStr;
+                if (connection.State == System.Data.ConnectionState.Closed)
+                {
+                    connection.Open();
+                }
+                string selectStr = "SELECT * FROM PRODUCTS";
+                SqlCommand cmd = new SqlCommand(selectStr, connection);
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    int id = int.Parse(rdr["PRODUCTID"].ToString());
+                    string name = rdr["NAME"].ToString();
+                    string description = rdr["DESCRIPTION"].ToString();
+                    decimal price = decimal.Parse(rdr["PRICE"].ToString());
+                    string img = rdr["ImageUrl"].ToString();
+                   // string createdAt = rdr["CREATED_AT_OF_PROD"].ToString().Substring(0, 10);
+
+                    //DateTime updateDate = new DateTime();
+
+                    //if (!string.IsNullOrEmpty(rdr["UPDATED_AT_OF_PROD"].ToString()))
+                    //{
+                    //    string updatedAt = rdr["UPDATED_AT_OF_PROD"].ToString().Substring(0, 10);
+                    //    updateDate = DateTime.ParseExact(updatedAt, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+                    //}
+
+                    //DateTime createDate = DateTime.ParseExact(createdAt, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+                    int typeID = int.Parse(rdr["CATEGORYID"].ToString());
+
+                    Admin_Product product = new Admin_Product(id, name, description, price, img, typeID);
+                    productList.Add(product);
+                }
+                if (connection.State == System.Data.ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+            }
+            return productList;
+        }
+
+        public void AddSql(string name, string description, decimal price, string img, int typeID)
+        {
+
+            using (SqlConnection connection = new SqlConnection(conStr))
+            {
+                if (connection.State == System.Data.ConnectionState.Closed)
+                {
+                    connection.Open();
+                }
+
+                string selectStr = "SET DATEFORMAT DMY;INSERT INTO PRODUCTS (NAME, DESCRIPTION, PRICE, ImageUrl, CATEGORYID)" +
+                    "VALUES (N'" + name + "' , N'" + description + "', " + price + ", N'" + img + "', '" + typeID + "')";
+                SqlCommand cmd = new SqlCommand(selectStr, connection);
+                cmd.ExecuteNonQuery();
+                if (connection.State == System.Data.ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+            }
+
+        }
+
+        public void DeleteSql(int id)
+        {
+            using (SqlConnection connection = new SqlConnection(conStr))
+            {
+                if (connection.State == System.Data.ConnectionState.Closed)
+                {
+                    connection.Open();
+                }
+                string selectStr = "DELETE PRODUCTS WHERE PRODUCTID = '" + id + "'";
+                SqlCommand cmd = new SqlCommand(selectStr, connection);
+                cmd.ExecuteNonQuery();
+                if (connection.State == System.Data.ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+            }
+        }
+
+        public bool checkProductInCart(int id)
+        {
+            using (SqlConnection connection = new SqlConnection(conStr))
+            {
+                if (connection.State == System.Data.ConnectionState.Closed)
+                {
+                    connection.Open();
+                }
+                string selectStr = "SELECT COUNT(*) FROM ShoppingCart JOIN PRODUCTS ON PRODUCTS.PRODUCTID = ShoppingCart.PRODUCTID WHERE PRODUCTS.PRODUCTID = " + id;
+                SqlCommand cmd = new SqlCommand(selectStr, connection);
+                int count = (int)cmd.ExecuteScalar();
+                if (connection.State == System.Data.ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+                return count > 0;
+            }
+        }
+
+    }
+}
